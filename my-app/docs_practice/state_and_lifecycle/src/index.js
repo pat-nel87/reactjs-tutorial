5a65fde2ac39c1e32f63f54c3307342445119ccc
pat-nel87 <patnelson2022@gmail.com>
2022-07-06 20:46:33 -0400

    Clock as Class - mount/unmount methods

--- my-app/docs_practice/state_and_lifecycle/src/index.js
+++ my-app/docs_practice/state_and_lifecycle/src/index.js
@@ -1,15 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
 
 class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
     render() {
         return (
         <div>
             <h1> Clock Demo </h1>
-            <h2> Time is {props.date.toLocaleTimeString()}.</h2>
+            <h2> Time is {this.state.date.toLocaleTimeString()}.</h2>
         </div>
     );
   }
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Clock />);
